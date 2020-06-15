6b5398ad57070c86fe06a54408afc261f0a6ea1b
Tien <viettienha98@gmail.com>
2020-06-15 11:33:16 +0700

    Dummy1

---
+++ train.py
@@ -0,0 +1,57 @@
+import mlflow
+import random
+import sys
+import time
+import argparse
+import pickle
+import json
+import os
+
+from model_builder.model import model_builder
+from dataset_builder.dataset import dataset_builder
+
+
+def save_model(name, params):
+    a_file = open(name, "wb")
+    pickle.dump(params, a_file)
+    a_file.close()
+
+
+def main():
+    epochs = 10
+    train_loss = 10.0
+    val_loss = 11.0
+    best_loss = 100.0
+
+    with open('config/dataset_config.json') as dc:
+        dataset_config = json.load(dc)
+
+    with open('config/model_config.json') as mc:
+        model_config = json.load(mc)
+
+    m = model_builder(model_config, retrain=False)
+    d = dataset_builder(dataset_config)
+
+    with mlflow.start_run():
+        mlflow.log_params(dataset_config)
+        mlflow.log_params(model_config)
+        mlflow.log_param('epochs', epochs)
+        for _ in range(epochs):
+            mlflow.log_metrics(
+                {'train_loss': train_loss, 'val_loss': val_loss})
+            train_loss *= random.randint(5, 12)/10
+            val_loss *= random.randint(5, 12)/10
+            if val_loss < best_loss:
+                best_loss = val_loss
+                # TODO:Save model here
+                save_model('DummyModel1.pkl', m.params)
+        # Save artifacts
+        mlflow.log_artifact('DummyModel1.pkl', 'weight')
+        mlflow.log_artifacts('dataset_builder', 'code/dataset')
+        mlflow.log_artifacts('model_builder', 'code/model')
+        mlflow.log_artifacts('config', 'code/config')
+        os.remove('DummyModel1.pkl')
+
+
+if __name__ == "__main__":
+    main()
